0ca965fc2488d32abee78b9ea3cdbad1efec5825
Steven S. <sschiavone@pace.edu>
2025-06-23 10:23:38 -0400

    Fixed window title

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class ConnectionApp:
     def __init__(self, root):
         logger.info("Initializing ConnectionApp")
         self.root = root
-        self.root.title("People Connection Visualizer")
+        self.root.title("COMRADE")
         self.root.geometry("1400x900")
         self.root.configure(bg=COLORS['background'])
         
